012a5a08b28b6d8a6323bb986c5c6726ee91babd
USUARIO <USUARIO@DESKTOP-M2HB9F5>
2022-09-05 17:17:27 -0700

    Agregar archivos de proyecto.

---
+++ CONSOLA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- PRESENTACION ---");
+            Console.WriteLine("MI NOMBRE ES SAMUEL");
+            Console.WriteLine("MI CIUDAD DE NACIEMIENTO ES MEXICALI");
+            Console.WriteLine("MI EDAD ES 21 AÑOS");
+            Console.ReadKey();
+        }
+    }
+}
